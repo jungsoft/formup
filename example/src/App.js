5cbcc327a2b18621bf861151113759eaa8818b82
Pedro Bini <dev.pedrobini@gmail.com>
2020-02-04 14:50:28 -0300

    Cleanup code

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'formup'
-
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div />
+);
