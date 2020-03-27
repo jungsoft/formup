54ee351d43b282c8cca4813c53951d7e8a585aea
Pedro Bini <dev.pedrobini@gmail.com>
2020-03-27 15:07:45 -0300

    Update FORMUP_INPUT_DANGER_CLASS_NAME

--- packages/core/src/constants/identifiers.ts
+++ packages/core/src/constants/identifiers.ts
@@ -2,4 +2,4 @@ export const FORMUP_FORM_CLASS_NAME = 'formup-form';
 
 export const FORMUP_INPUT_CLASS_NAME = 'formup-input';
 
-export const FORMUP_INPUT_DANGER_CLASS_NAME = 'formup-danger';
+export const FORMUP_INPUT_DANGER_CLASS_NAME = 'formup-input-danger';
