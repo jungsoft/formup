13b2b7205bd4ae8a308b23c0687a4eec742072fb
Pedro Bini <dev.pedrobini@gmail.com>
2020-03-27 11:13:19 -0300

    Describe interfaces

--- packages/core/src/interfaces/index.ts
+++ packages/core/src/interfaces/index.ts
@@ -4,19 +4,31 @@ import { FormikConfig } from 'formik';
 import { FormInputProps } from '../components/FormInput/FormInput';
 import { FormProps } from '../components/Form/Form';
 
+/**
+ * Yup schema as used by Formup.
+ */
 export interface YupSchema extends yup.Schema<any> {
   fields: object;
 }
 
+/**
+ * Options for validateForm.
+ */
 export interface ValidateFormOptions extends yup.ValidateOptions {
   paths?: string | string[],
 }
 
+/**
+ * Options for useFormup.
+ */
 export interface UseFormupOptions extends FormikConfig<any> {
   onError?: (errors: string[]) => void,
   onSubmit: (values: object) => void,
 }
 
+/**
+ * Result returned by useFormup.
+ */
 export interface UseFormupResult {
   FormInput: React.FunctionComponent<FormInputProps>,
   Form: React.FunctionComponent<FormProps>,
