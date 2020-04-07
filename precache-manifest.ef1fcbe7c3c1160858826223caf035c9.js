aafb8255942af3741c36128f70c4f45a6ca75480
Pedro Bini <dev.pedrobini@gmail.com>
2020-04-07 12:37:16 -0300

    Updates

---
+++ precache-manifest.ef1fcbe7c3c1160858826223caf035c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffaacacf650527339c47ce898f89c4f0",
+    "url": "/formup/index.html"
+  },
+  {
+    "revision": "6a99a9513dcc6a415e96",
+    "url": "/formup/static/css/main.1593959f.chunk.css"
+  },
+  {
+    "revision": "fe8d6c20cfa28fe48219",
+    "url": "/formup/static/js/2.b580bbd6.chunk.js"
+  },
+  {
+    "revision": "352d9ca6aef64a51fabd034e988f8ec2",
+    "url": "/formup/static/js/2.b580bbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a99a9513dcc6a415e96",
+    "url": "/formup/static/js/main.d5f9f306.chunk.js"
+  },
+  {
+    "revision": "9956d54f579e2bce50cf",
+    "url": "/formup/static/js/runtime-main.db7e77a7.js"
+  }
+]);
