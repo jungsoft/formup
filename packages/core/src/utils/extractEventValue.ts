b164f0cf14493cb8a1e01ebb78b7f8ef0bd4115e
Pedro Bini <dev.pedrobini@gmail.com>
2020-04-15 22:53:48 -0300

    Fix extractEventValue glitch when clearing input

--- packages/core/src/utils/extractEventValue.ts
+++ packages/core/src/utils/extractEventValue.ts
@@ -2,6 +2,10 @@
  * Returns an event target value, or the value itself.
  * @param event Event
  */
-const extractEventValue = (event: any) => event?.target?.value || event;
+const extractEventValue = (event: any) => (
+  event?.target
+    ? event?.target?.value
+    : event
+);
 
 export default extractEventValue;
