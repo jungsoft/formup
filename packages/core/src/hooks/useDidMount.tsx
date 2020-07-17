701b44e0c21abf7ad8b7ef3658b76763253d8689
Pedro Bini <dev.pedrobini@gmail.com>
2020-07-17 17:40:22 -0300

    Suppress lint warnings

--- packages/core/src/hooks/useDidMount.tsx
+++ packages/core/src/hooks/useDidMount.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
  * Hook that executes a callback when the component mounts.
  */
 const useDidMount = (callback: () => void) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(callback, []);
 };
 
