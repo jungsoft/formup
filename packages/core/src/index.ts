17b8c736db2670b5abdaf058b72885d971e7376b
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2020-07-31 11:09:14 -0300

    Add export components

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,19 @@
 import mapInitialValuesFromSchema from './utils/mapInitialValuesFromSchema';
 import createSchema from './yup/createSchema';
 import useFormup from './hooks/useFormup';
+import Form from './components/Form/Form';
+import FormInput from './components/FormInput/FormInput';
+import FormArrayField from './components/FormArrayField/FormArrayField';
+import FormInputGroup from './components/FormInputGroup/FormInputGroup';
+import FormInputGroupItem from './components/FormInputGroupItem/FormInputGroupItem';
 
 export {
   useFormup,
   createSchema,
   mapInitialValuesFromSchema,
+  Form,
+  FormInput,
+  FormArrayField,
+  FormInputGroup,
+  FormInputGroupItem,
 };
